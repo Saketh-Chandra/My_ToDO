47985ee184ee31c8e2cd5cd5cef876183badb6d6
Saketh Chandra <b.sakethchandra9@gmail.com>
2020-10-27 13:11:24 +0530

    edited update templates and delete templates!

--- My_ToDO/settings.py
+++ My_ToDO/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e^$9mt#)3niaz9b9pyxc1x(53&qq1^@iuy2)=5hhji1t+*o_0$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False #True
+DEBUG = True
 
-ALLOWED_HOSTS = ['my--todo.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
