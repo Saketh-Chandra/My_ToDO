2918db5c8610931bc4db419fe45dc19a69f32eb0
Saketh Chandra <b.sakethchandra9@gmail.com>
2021-08-24 10:27:32 +0530

    minor bug fixes

--- My_ToDO/settings.py
+++ My_ToDO/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY', 'e^$9mt#)3niaz9b9pyxc1x(53&qq1^@iuy2)=5hhji1t+*o_0$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = os.getenv('DEBUG', False) == 'True' # os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,5 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = '/static/' # os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/static/'  # os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
